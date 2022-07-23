63bc6e186cf6fd2185b98831d128cb6a2905c37b
Alireezajb <alirezajabberi@gmail.com>
2022-07-23 13:33:08 +0430

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { weatherData } from './models/weather.model';
+import { WeatherService } from './Services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  cityName: string = 'ahvaz';
+  weatherData?: weatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+
+
+  }
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe
+      ({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response);
+        }
+      });
+  }
+}
